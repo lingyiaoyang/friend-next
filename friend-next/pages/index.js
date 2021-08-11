9180b61d92cc1a15830eff834e33c6f048b7fe7d
lingyiaoyang <76722905+lingyiaoyang@users.noreply.github.com>
2021-08-11 19:19:55 +0800

    upload again

--- friend-next/pages/index.js
+++ friend-next/pages/index.js
@@ -1,8 +1,3 @@
-import Head from 'next/head';
-import Image from 'next/image';
-//component
-import Navigation from '../component/nav';
-
 export default function Home() {
   return <div>Home page</div>;
 }
